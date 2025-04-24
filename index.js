7bba0bd256a8fbb7eca8249e3daac668affc700d
Yang96z <cheeyong995@hotmail.com>
2025-04-24 13:35:29 +0800

    getting started with express.js

---
+++ index.js
@@ -0,0 +1,31 @@
+// install express for us to run this project 
+// express allow us to create end-points to listen for incoming request
+
+// implies that the code written here uses strict rules
+'use strict';
+
+const express = require("express"); //import express
+
+//constants
+const PORT = 8888;                  //port address that the app is listening , CTRL+C to stop server and rerun once change port
+const HOST = "127.0.0.1";           //localhost, default, http://127.0.0.1:8080 == http://localhost:8080
+const OS = require("os");         //import the os env.library
+const ENV = "DEV";                  //specify that this project is under Development
+
+// app
+
+const app = express();                           //invoke the express library
+app.get("/", (req, res)=>{                       //listn for get request: http://127.0.0.1/, callback function processes the request
+    res.statusCode = 200;                        //return 200 status code === SUCCESS
+    const msg = "Hello FSD learners"             //create the response message
+    res.send(msg);
+})
+
+app.get("/test", (request, response)=>{         //listen for get request: http://127.0.0.1/test
+    response.statusCode = 200;
+    const msg = "Hello from /test Node!";
+    response.send(msg);
+});
+
+app.listen(PORT, HOST);                             //invoke the listen() method for app, listen to port 8080, host:127.0.0.1
+console.log(`Running on http://${HOST}:${PORT}`); //log the start of the app to listen for requests      
